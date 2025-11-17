e383fa3b50559a7982d6548ea9059913fc144ac8
mik <michelemolinaro026@gmail.com>
2025-11-17 09:29:54 +0100

    aggiunto file di una prova esame

---
+++ programmazione/proveEsame/18Febbraio2025/parrucchiere.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CodaDalParrucchiere{
+	private:
+			vector<string> parrucchieri;
+	public:
+			CodaDalParrucchiere() : parrucchieri(parrucchieri){}
+
+			void aggiungiPrenotazione
+};
+
+
+int main(){
+	return 0;
+}
