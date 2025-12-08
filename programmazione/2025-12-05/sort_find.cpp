f6efdf2ca7acb623f6d3f118d7fcff42d49aed68
mik <michelemolinaro026@gmail.com>
2025-12-08 11:18:10 +0100

    aggiunti esercizi e slides

--- programmazione/2025-12-05/sort_find.cpp
+++ programmazione/2025-12-05/sort_find.cpp
@@ -12,43 +12,56 @@ class Prodotto{
     
     Prodotto(int id, string nome):id(id),nome(nome){}
     
-   /* bool operator<(const Prodotto& p2){
+    bool operator<(const Prodotto& p2){
         return nome<p2.nome;
-    }*/
-    /*bool operator==(const Prodotto& p2){
+    }
+    bool operator==(const Prodotto& p2){
         return nome==p2.nome&&id==p2.id;
-    }*/
+    }
+
+    // operatore di stampa
     friend ostream & operator<<(ostream & o, const Prodotto & p){
-        o<<p.id<<p.nome;
+        o << p.id << " " << p.nome;
         return o;
     }
+
+    int getId(){return id;}
     string getNome()const{return nome;}
     
 };
 
-bool mycmp(const Prodotto& p1, const Prodotto& p2){
-    return p1.getNome()>p2.getNome();
-    
-}
+bool mycmp(const Prodotto& p1, const Prodotto& p2){return p1.getNome() < p2.getNome();}
+
 int main()
 {
-    Prodotto p1(10,"Sale");
-    Prodotto p2(1,"Pepe");
-    Prodotto p3(1,"Sale");
-    vector<Prodotto> v={p1,p2};
-    for(Prodotto p:v){
-        cout <<p  << endl;
-        
+    Prodotto p1(1,"Sugo");
+    Prodotto p2(2,"Pesto");
+    Prodotto p3(3,"Amatriciana");
+    Prodotto p4(4, "Lasagna");
+
+    vector<Prodotto> v = {p1,p2,p3,p4};
+    
+    for(Prodotto p : v){
+        cout << p << endl;
     }
+
     sort(v.begin(),v.end(),mycmp);
+
+    cout << endl;
+
     for(Prodotto p:v){
-        cout <<p  << endl;
-        
+        cout << p << endl;
     }
-    vector<Prodotto>::iterator it=find(v.begin(),v.end(),p3);
-    if(it!=v.end())
-        cout<<*(it);
+
+    cout << endl;
+
+    cout << "Find di un prodotto:" << endl;
+    vector<Prodotto>::iterator it = find(v.begin(), v.end(), p1);
+    if(it != v.end())
+        cout << *(it);
     else
-        cout<<"not found";
+        cout << "not found";
+
+    cout << endl;
     return 0;
 }
