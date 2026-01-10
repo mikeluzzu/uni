6b7e75a93cb78220913c26ebcaa66d54a0db827a
michele <mik@pop-os.homenet.telecomitalia.it>
2026-01-10 08:55:15 +0100

    aggiunto un esercizio esame

---
+++ programmazione/tracce d'esame/2025-07-10/2es/OrganizzaRecensioni.h
@@ -0,0 +1,23 @@
+#ifndef ORGANIZZARECENSIONI_H
+#define ORGANIZZARECENSIONI_H
+using namespace std;
+#include <iostream>
+#include <string>
+#include <map>
+class OrganizzaRecensioni{
+
+    private:
+        string nome;
+        string localita;
+        <pair<double, double> coordinate;
+        string recensione;
+
+        bool aggiungiRecensione(string& rec){
+            
+        }
+
+        
+
+};
+
+#endif
